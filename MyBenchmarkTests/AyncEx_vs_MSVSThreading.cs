63ec0aa39f1ab488ec9756fb60acbd5894a6520b
이장호 <libu1129@naver.com>
2023-03-23 21:48:22 +0900

    nito.asyncex쪽이 조금떠 빠르다

--- MyBenchmarkTests/AyncEx_vs_MSVSThreading.cs
+++ MyBenchmarkTests/AyncEx_vs_MSVSThreading.cs
@@ -1,5 +1,8 @@
-﻿using System;
+﻿using BenchmarkDotNet.ConsoleArguments.ListBenchmarks;
+
+using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +11,46 @@ namespace MyBenchmarkTests
 {
     internal class AyncEx_vs_MSVSThreading
     {
+        int run_count = 1000;
 
+
+        public async Task test()
+        {
+            List<long> elapsed_list = new List<long>();
+            for (int i = 0; i < 100; i++)
+            {
+                var elapsed = await run();
+                elapsed_list.Add(elapsed);
+            }
+            var avg = elapsed_list.Average();
+
+            Debug.WriteLine($"elapsed ticks : {avg.ToString("n0")}");
+        }
+
+        /// <summary>
+        /// nito asyncex 쪽이 조금더 빠르다
+        /// </summary>
+        /// <returns></returns>
+        private async Task<long> run()
+        {
+            //Nito.AsyncEx.AsyncManualResetEvent are = new ();
+            Microsoft.VisualStudio.Threading.AsyncManualResetEvent are = new();
+
+            Stopwatch st = new Stopwatch();
+
+            _ = Task.Run(async () =>
+            {
+                await are.WaitAsync().ConfigureAwait(false);
+                st.Stop();
+                var elapsed_ticks = st.ElapsedTicks;
+            }).ConfigureAwait(false);
+
+            await Task.Delay(100);
+
+            st.Restart();
+            are.Set();
+
+            return st.ElapsedTicks;
+        }
     }
 }
--- MyBenchmarkTests/Program.cs
+++ MyBenchmarkTests/Program.cs
@@ -1,2 +1,9 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using MyBenchmarkTests;
+
 Console.WriteLine("Hello, World!");
+
+
+AyncEx_vs_MSVSThreading tester = new AyncEx_vs_MSVSThreading();
+await tester.test();
+
